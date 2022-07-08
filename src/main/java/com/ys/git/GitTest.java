ffdcd3fb59238ee7d0f10bdb693ddb356e4ccd9c
yan-sha <58388873+yan-sha@users.noreply.github.com>
2022-07-08 20:15:35 +0800

    pull

--- src/main/java/com/ys/git/GitTest.java
+++ src/main/java/com/ys/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello github4!");
         System.out.println("hello github4 master!");
         System.out.println("hello push!");
+        System.out.println("hello pull!");
     }
 }
