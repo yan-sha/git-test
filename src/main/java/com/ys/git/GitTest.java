6e7cfa103c299451d69696f4bc4367d74465cb8b
Ys <Ys@123.com>
2022-07-08 18:06:23 +0800

    hot-fix commit2

--- src/main/java/com/ys/git/GitTest.java
+++ src/main/java/com/ys/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello github!");
         System.out.println("hello github2!");
         System.out.println("hello github3!");
+        System.out.println("hello github4!");
     }
 }
